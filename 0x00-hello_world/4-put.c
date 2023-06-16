9c09a12fbd9d21d1b9daf7e37407a3ad0ef0dc88
Kizito72 <kizitorawbiz@gmail.com>
2023-06-15 20:37:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//This is a programm that uses pus to print
+
+int main{
+	puts("Programming is like building a multilingual puzzle");
+
+	return 0;  //If 0 is returned, it means our script is correct
+
+}
